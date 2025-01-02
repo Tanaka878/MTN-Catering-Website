e089ad751501a63eea5e813c475619b9bb1712ca
Tanaka878 <musungaretanaka@gmail.com>
2025-01-02 11:48:04 +0200

    commit

---
+++ src/app/sections/Header/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-center bg-black text-white text-sm py-3 w-full h-1'>
+        
+
+      
+    </div>
+  )
+}
+
+export default Header

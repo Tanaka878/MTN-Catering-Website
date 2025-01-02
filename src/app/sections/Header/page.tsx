913ab20fdf3881d440888191699ba273c3ef7291
Tanaka878 <musungaretanaka@gmail.com>
2025-01-02 15:40:59 +0200

    working on header

--- src/app/sections/Header/page.tsx
+++ src/app/sections/Header/page.tsx
@@ -1,13 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
-
+import {FiMenu} from 'react-icons/fi'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-center bg-black text-white text-sm py-3 w-full h-1'>
-        
+    <div className="flex justify-between items-center py-1 w-full">
+  <Image
+    src="/Images/catering.png"
+    height={50}
+    width={70}
+    alt="Catering Logo"
+  />
 
-      
-    </div>
+  <FiMenu className="h-5 w-5 text-slate-600 cursor-pointer md:hidden" />
+</div>
   )
 }
 
